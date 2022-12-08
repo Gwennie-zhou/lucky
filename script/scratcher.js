806ed177833303aa168be78bcb39da7f2e9dbabd
Gwennie-zhou <Gwennie_zhou@126.com>
2022-12-08 21:51:58 +0800

    微调样式

--- script/scratcher.js
+++ script/scratcher.js
@@ -39,7 +39,7 @@ function scratcher() {
       const x = e.pageX - scratcherWrap.offsetLeft + scratcherWrap.offsetWidth / 2
       const y = e.pageY - scratcherWrap.offsetTop - scratcherWrap.offsetHeight / 2
       ctx.beginPath() 
-      ctx.arc(x,y, 20, 0, 2 * Math.PI)
+      ctx.arc(x,y, 20, 0, 2 * Math.PI) // 这里的x和y是相对于canvas画布
       ctx.globalCompositeOperation = "destination-out"
       ctx.fill()
       ctx.closePath()
