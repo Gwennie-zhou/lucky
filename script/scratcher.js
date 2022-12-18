dbb61672c9175f698d7dd53b0b9490e291884e8c
youzi <w_zequan@163.com>
2022-12-18 12:14:58 +0800

    feat: 表白的画布，改为touch事件，设置画布大小。

--- script/scratcher.js
+++ script/scratcher.js
@@ -16,9 +16,10 @@ export function scratcher() {
           <span>你愿意与我一同开启我们的初恋吗</span>
         </div>
       </div>
-      <canvas id="canvas"></canvas>
+      <canvas id="canvas" width="375px" height="350px"></canvas>
     </div>
   `
+// canvas 标签有width 和 height属性，需要设置，如果不设置，好像默认都是是100px，所以操作画布的时候，跟我们文档流的坐标系会有所出入。
 
   const scratcherWrap = document.querySelector('.scratcher-wrap')
   const canvas = document.querySelector('#canvas')
@@ -31,22 +32,26 @@ export function scratcher() {
     e.preventDefault();
   })
 
+  // 移动端，改为touch事件（记住哈，爱你）
   let isDraw = false;
-  canvas.addEventListener('mousedown', ()=>{
+  canvas.addEventListener('touchstart', ()=>{
     isDraw = true
+    console.log('touchstart', isDraw)
   })
-  canvas.addEventListener('mousemove',move)
-  canvas.addEventListener('mouseup',()=>{
+  canvas.addEventListener('touchmove',move)
+  canvas.addEventListener('touchend',()=>{
     isDraw = false
+    console.log('touchend', isDraw)
   })
 
   function move(e) {
     if (isDraw) {
-      console.log(e);
-      const x = e.pageX
-      const y = e.pageY - scratcherWrap.offsetTop
+      const local = e.touches[0];
+      const x = local.pageX
+      // 我们需要知道点击的坐标，但它是相对于整个页面的，我们需要让他相对于画布，所以坐标的y需要减去画布上面的那部分高度
+      const y = local.pageY - (document.body.offsetHeight - scratcherWrap.offsetHeight)
       ctx.beginPath() 
-      ctx.arc(x,y, 20, 0, 2 * Math.PI) // 这里的x和y是相对于canvas画布
+      ctx.arc(x, y, 15, 0, 2 * Math.PI) // 这里的x和y是相对于canvas画布
       ctx.globalCompositeOperation = "destination-out"
       ctx.fill()
       ctx.closePath()
